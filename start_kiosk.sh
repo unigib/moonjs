f974b046eba3c5f4ec2bb9f60f08f78606bd8d1c
University of Gibraltar <dave.robertson@unigib.edu.gi>
2026-09-13 10:46:26 +0100

    Create start_kiosk.sh
    Added a kiosk mode

---
+++ start_kiosk.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# 1. Start the Python web server in the background
+# Source - https://stackoverflow.com/a/55053143
+# Posted by eddyizm, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-09-13, License - CC BY-SA 4.0
+
+python3 -m http.server 8000 --bind 127.0.0.1 &
+
+
+# 2. Wait 3 seconds for the server to spin up and bind to the port
+sleep 3
+
+# 3. Launch Chromium in kiosk mode pointing to localhost
+chromium --kiosk --password-store=basic --noerrdialogs --disable-infobars --check-for-update-interval=31536000 http://localhost:8000
+
